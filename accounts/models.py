5d04aa58c137fd9a94705ad49a302970f3c3cc5b
JackLamb99 <jacklamb15@yahoo.co.uk>
2025-10-27 17:09:38 +0000

    ADD custom user model with email as the unique identifier

---
+++ accounts/models.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    """
+    Configures Django User model to use email as the unique identifier.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("An email address must be provided.")
+        if not password:
+            raise ValueError("A password must be provided.")
+        email = self.normalize_email(email).lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Simplified custom user model with email as login.
+    """
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
