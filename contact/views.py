879cc37a93bf99ca96f2602c0f08b7321c425ae3
JackLamb99 <jacklamb15@yahoo.co.uk>
2025-10-31 00:52:57 +0000

    ADD contact view to handle message submissions and user prefill

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import ContactMessageForm
 
-# Create your views here.
+
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Thanks! Your message has been sent.")
+            return redirect("contact:contact")
+    else:
+        # Prefill for logged-in users
+        initial = {}
+        if request.user.is_authenticated:
+            initial = {
+                "email": getattr(request.user, "email", "") or "",
+                "first_name": getattr(request.user, "first_name", "") or "",
+                "last_name": getattr(request.user, "last_name", "") or "",
+            }
+        form = ContactMessageForm(initial=initial)
+
+    return render(request, "contact/contact.html", {"form": form})
