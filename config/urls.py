68f50ceaebc6e7de258cf6bbf9e625f519ce93d0
JackLamb99 <jacklamb15@yahoo.co.uk>
2025-10-31 00:50:57 +0000

    UPDATE urls.py to add namespace to services, bookings, and contact URL patterns

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
-    path("services/", include("services.urls")),
-    path("bookings/", include("bookings.urls")),
+    path("services/", include("services.urls", namespace="services")),
+    path("bookings/", include("bookings.urls", namespace="bookings")),
+    path("contact/", include("contact.urls", namespace="contact")),
 ]
